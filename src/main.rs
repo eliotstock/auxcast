a73a39aee7d07af83f67c836f9f4119c9df7f652
Eliot Stock <1144814+eliotstock@users.noreply.github.com>
2025-04-30 12:58:49 +1200

    No sound

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use cast_sender;
 use cast_sender::namespace::media::{
     MediaInformationBuilder, MusicTrackMediaMetadataBuilder, StreamType,
 };
-use cast_sender::{AppId, Error, ImageBuilder, MediaController, Receiver};
+use cast_sender::{AppId, Error, MediaController, Receiver};
 use anyhow::{Context, Result};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::{SampleFormat, Stream};
@@ -14,14 +14,18 @@ use std::io::{self, Read};
 use anyhow;
 use cpal;
 
-const CHROMECAST_IP_A: &str = "192.168.86.247"; // works
+// Get these from the Home app on your phone
+// TODO: How do we send to a speaker group?
+const CHROMECAST_IP_A: &str = "192.168.86.20"; // works
 const CHROMECAST_IP_B: &str = "192.168.86.29"; // works
 const CHROMECAST_IP_C: &str = "192.168.86.238"; // works
 
 const CHROMECAST_IP: &str = CHROMECAST_IP_C;
 
-const STREAM_IP: &str = "127.0.0.1";
-const STREAM_PORT: &str = "8080";
+// To find this on a Mac, assuming we're using WiFi:
+// ipconfig getifaddr en0
+const STREAM_IP: &str = "192.168.86.247";
+const STREAM_PORT: u16 = 8080;
 
 async fn cast(stream_port: u16) -> Result<(), Error> {
     let receiver = Receiver::new();
@@ -36,7 +40,7 @@ async fn cast(stream_port: u16) -> Result<(), Error> {
         .unwrap();
 
     // Create a streaming URL that points to our local server
-    let stream_url = format!("http://{}:{}/stream", STREAM_IP, stream_port);
+    let stream_url = format!("http://{}:{}", STREAM_IP, stream_port);
     
     let media_info = MediaInformationBuilder::default()
         .content_id(&stream_url)
@@ -49,131 +53,6 @@ async fn cast(stream_port: u16) -> Result<(), Error> {
     media_controller.load(media_info).await?;
     Ok(())
 }
-
-// async fn get_external_ip() -> Result<String, reqwest::Error> {
-//     let response = reqwest::get("https://api.ipify.org").await?;
-//     response.text().await
-// }
-
-// fn main() -> Result<(), anyhow::Error> {
-//     // Get local IP first
-//     let ip = local_ip().unwrap();
-//     let stream_address = format!("{}:{}", STREAM_IP, STREAM_PORT);
-//     let listener = TcpListener::bind(&stream_address)?;
-//     println!("Server listening on {}:{}", STREAM_IP, STREAM_PORT);
-
-//     let available_hosts = cpal::available_hosts();
-//     println!("Available hosts:\n  {:?}", available_hosts);
-
-//     for host_id in available_hosts {
-//         println!("{}", host_id.name());
-//         let host = cpal::host_from_id(host_id)?;
-
-//         let default_in = host.default_input_device().map(|e| e.name().unwrap());
-//         let default_out = host.default_output_device().map(|e| e.name().unwrap());
-//         println!("  Default Input Device:\n    {:?}", default_in);
-//         println!("  Default Output Device:\n    {:?}", default_out);
-
-//         let devices = host.devices()?;
-//         println!("  Devices: ");
-//         for (device_index, device) in devices.enumerate() {
-//             println!("  {}. \"{}\"", device_index + 1, device.name()?);
-
-//             let mut supported_configs_range = device.supported_output_configs()
-//                 .expect("error while querying configs");
-
-//             // This should be this one on my machine:
-//             // SupportedStreamConfigRange { channels: 20, min_sample_rate: SampleRate(44100), max_sample_rate: SampleRate(44100), buffer_size: Range { min: 14, max: 4096 }, sample_format: F32 }
-//             let supported_config = supported_configs_range.next()
-//                 .expect("no supported config?!")
-//                 .with_max_sample_rate();
-//             println!("  Stream config: {:?}. ", supported_config);
-
-//             let stream = device.build_output_stream(
-//                 &supported_config.into(),
-//                 move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-//                     // react to stream events and read or write stream data here.
-//                 },
-//                 move |err| {
-//                     // react to errors here.
-//                 },
-//                 None // None=blocking, Some(Duration)=timeout
-//             )?;
-//             // println!("  Stream: {:?}. ", stream.);
-
-//             stream.play()?;
-
-//             for stream in listener.incoming() {
-//                 println!("Incoming connection");
-                
-//                 let mut stream = stream?;
-//                 let response = "HTTP/1.1 200 OK\r\nContent-Type: audio/wav\r\n\r\n";
-//                 stream.write_all(response.as_bytes())?;
-
-//                 // Send audio data to the client
-//                 // This is a simplified example and doesn't include proper WAV headers
-//                 loop {
-//                     let buffer = vec![0.0f32; 1024];
-//                     // Fill buffer with audio data from the input stream
-//                     stream.write_all(bytemuck::cast_slice(&buffer))?;
-//                 }
-//             }
-
-//             // // Input configs
-//             // if let Ok(conf) = device.default_input_config() {
-//             //     println!("    Default input stream config:\n      {:?}", conf);
-//             // }
-//             // let input_configs = match device.supported_input_configs() {
-//             //     Ok(f) => f.collect(),
-//             //     Err(e) => {
-//             //         println!("    Error getting supported input configs: {:?}", e);
-//             //         Vec::new()
-//             //     }
-//             // };
-//             // if !input_configs.is_empty() {
-//             //     println!("    All supported input stream configs:");
-//             //     for (config_index, config) in input_configs.into_iter().enumerate() {
-//             //         println!(
-//             //             "      {}.{}. {:?}",
-//             //             device_index + 1,
-//             //             config_index + 1,
-//             //             config
-//             //         );
-
-//             //         if device_index == 0 && config_index == 0 {
-                
-//             //             // stream.play()?;
-//             //         }
-//             //     }
-//             // }
-
-//             // // Output configs
-//             // if let Ok(conf) = device.default_output_config() {
-//             //     println!("    Default output stream config:\n      {:?}", conf);
-//             // }
-//             // let output_configs = match device.supported_output_configs() {
-//             //     Ok(f) => f.collect(),
-//             //     Err(e) => {
-//             //         println!("    Error getting supported output configs: {:?}", e);
-//             //         Vec::new()
-//             //     }
-//             // };
-//             // if !output_configs.is_empty() {
-//             //     println!("    All supported output stream configs:");
-//             //     for (config_index, config) in output_configs.into_iter().enumerate() {
-//             //         println!(
-//             //             "      {}.{}. {:?}",
-//             //             device_index + 1,
-//             //             config_index + 1,
-//             //             config
-//             //         );
-//             //     }
-//             // }
-//         }
-//     }
-
-//     Ok(())
-// }
 
 fn stream_from_input() -> Result<u16> {
     println!("Starting audio streaming server...");
@@ -241,7 +120,7 @@ fn stream_from_input() -> Result<u16> {
     println!("Audio stream started");
     
     // Define the target - this is where we'll send the audio data
-    let target = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let target = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), STREAM_PORT);
     println!("Streaming audio to {}", target);
     
     // Set up a thread for network sending
@@ -250,6 +129,7 @@ fn stream_from_input() -> Result<u16> {
     
     // Create a separate thread for sending data to avoid blocking the main thread
     let _sender_thread = thread::spawn(move || {
+        const MAX_PACKET_SIZE: usize = 1400; // Conservative UDP packet size
         loop {
             let data = {
                 match buffer_clone.lock() {
@@ -272,8 +152,13 @@ fn stream_from_input() -> Result<u16> {
             };
             
             if !data.is_empty() {
-                if let Err(e) = socket_clone.send_to(&data, target) {
-                    eprintln!("Failed to send data: {}", e);
+                // Split the data into chunks of MAX_PACKET_SIZE
+                for chunk in data.chunks(MAX_PACKET_SIZE) {
+                    if let Err(e) = socket_clone.send_to(chunk, target) {
+                        eprintln!("Failed to send data chunk: {}", e);
+                    }
+                    // Small delay between chunks to avoid overwhelming the network
+                    thread::sleep(Duration::from_micros(100));
                 }
             }
         }
